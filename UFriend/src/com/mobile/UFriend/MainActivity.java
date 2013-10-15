714b7b85e28f6ce17fcd9dd10bf6a37d8ce54b5d
puding <puding1507@gmail.com>
2013-10-15 16:32:52 +0900

    소스갱신

--- UFriend/src/com/mobile/UFriend/MainActivity.java
+++ UFriend/src/com/mobile/UFriend/MainActivity.java
@@ -1,30 +1,76 @@
 package com.mobile.UFriend;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+
+    private ProgressDialog loagindDialog;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
 
-        //Hello World!
+        createThreadAndDialog();
 
-
-        new MainAsync().execute("");
+        //new MainAsync().execute("");
 
 
     }
 
-    public class MainAsync extends AsyncTask <String, String, String>
-    {
+    void createThreadAndDialog() {
+        /* ProgressDialog */
+        loagindDialog = ProgressDialog.show(this, "title", "Loading", true, true);
+        loagindDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                // 시간걸리는 처리
+                //handler.sendEmptyMessage(5000);
+            }
+        });
+        thread.start();
+    }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            loagindDialog.dismiss(); // 다이얼로그 삭제
+            // View갱신
+        }
+    };
+
+    /*
+    private Dialog busyDialog;
+
+    public void showBusyDialog(String message) {
+        busyDialog = new Dialog(this, R.style.lightbox_dialog);
+        busyDialog.setContentView(R.layout.lightbox_dialog);
+        ((TextView)busyDialog.findViewById(R.id.dialogText)).setText(message);
+
+        busyDialog.show();
+    }
+
+    public void dismissBusyDialog() {
+        if (busyDialog != null)
+            busyDialog.dismiss();
+
+        busyDialog = null;
+    }
+    */
+
+    public class MainAsync extends AsyncTask<String, String, String> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();    //To change body of overridden methods use File | Settings | File Templates.
